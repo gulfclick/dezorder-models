0783c1f5a0f80d29bd559dc7287e58fc4206cf49
HeroGroup <navid_hero_1st@yahoo.com>
2019-08-07 12:26:53 +0430

    add settings

---
+++ src/Setting.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace CargoLogisticsModels;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Setting extends Model
+{
+    protected $fillable = [
+        'setting_key', 'setting_value', 'setting_placeholder'
+    ];
+}
