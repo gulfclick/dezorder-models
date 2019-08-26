7d225110b305c03053b122580a864c1c51d386b2
development@gulfclick.net <development@gulfclick.net>
2019-08-26 14:30:24 +0430

    soft delete

--- src/JobItem.php
+++ src/JobItem.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class JobItem extends Model
 {
+	use SoftDeletes;
+	
     protected $fillable = [
         'job_id', 'quantity', 'item_description', 'item_price'
     ];
