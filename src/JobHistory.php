94696cf58567946927423ad25ea02439f62cbbdc
development@gulfclick.net <navid_hero_1st@yahoo.com>
2019-09-16 14:56:28 +0430

    add distance_to_dropoff to jobHistory

--- src/JobHistory.php
+++ src/JobHistory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class JobHistory extends Model
 {
     protected $fillable = [
-        'job_id', 'driver_id', 'driver_latitude', 'driver_longitude', 'distance_to_pickup', 'status', 'duration_minutes'
+        'job_id', 'driver_id', 'driver_latitude', 'driver_longitude', 'distance_to_pickup', 'distance_to_dropoff', 'status', 'duration_minutes'
     ];
 
     public function job()
@@ -19,4 +19,9 @@ class JobHistory extends Model
     {
         return $this->belongsTo(Driver::class);
     }
+	
+	public function getDistanceToPickupAttribute($value)
+	{
+		return round($value/1000, 2, PHP_ROUND_HALF_UP);
+	}
 }
